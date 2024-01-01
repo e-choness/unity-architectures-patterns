27696d5e3227db8794892f88571e312cbac16842
Echo Yin <echo_ybl@hotmail.com>
2024-01-01 16:38:42 -0500

    Add injector details

--- Assets/LightWeightDI/Scripts/Injector.cs
+++ Assets/LightWeightDI/Scripts/Injector.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 
 namespace DependencyInjection
@@ -23,9 +26,49 @@ namespace DependencyInjection
 
     public interface IDependencyProvider {}
 
-    public class Injector : MonoBehaviour
+    public class Injector : Singleton<Injector>
     {
+        private const BindingFlags ProviderBindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+
+        private readonly Dictionary<Type, object> _registry = new();
+
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            // Find all modules implementing IDependencyProvider
+            var providers = FindMonoBehaviours().OfType<IDependencyProvider>();
+            foreach (var provider in providers)
+            {
+                RegisterProvider(provider);
+            }
+        }
+
+        void RegisterProvider(IDependencyProvider provider)
+        {
+            var methods = provider.GetType().GetMethods(ProviderBindingFlags);
+
+            foreach (var method in methods)
+            {
+                if(!Attribute.IsDefined(method, typeof(ProvideAttribute))) continue;
+
+                var returnType = method.ReturnType;
+                var providerInstance = method.Invoke(provider, null);
+                if (providerInstance != null)
+                {
+                    _registry.Add(returnType, providerInstance);
+                    Debug.Log($"Register {returnType.Name} from {provider.GetType().Name}");
+                }
+                else
+                {
+                    throw new Exception($"Provider {provider.GetType().Name} return null for {returnType.Name}");
+                }
+            }
+        }
         
+        static MonoBehaviour[] FindMonoBehaviours()
+        {
+            return FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.InstanceID);
+        }
     }
-    
 }
