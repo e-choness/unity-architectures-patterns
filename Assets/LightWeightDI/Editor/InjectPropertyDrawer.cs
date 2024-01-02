b13664f433ed6f7b3feb39e6fafb9baa35e23e33
Echo Yin <echo_ybl@hotmail.com>
2024-01-02 14:05:40 -0500

    Added Injector in-editor tools

---
+++ Assets/LightWeightDI/Editor/InjectPropertyDrawer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+namespace DependencyInjection
+{
+    [CustomPropertyDrawer(typeof(InjectAttribute))]
+    public class InjectPropertyDrawer : PropertyDrawer
+    {
+        private Texture2D injectorIcon;
+
+        Texture2D LoadIcon()
+        {
+            if (injectorIcon == null)
+            {
+                injectorIcon = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/LightWeightDI/Icons/injector-icon.png");
+            }
+            return injectorIcon;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            injectorIcon = LoadIcon();
+            var iconRect = new Rect(position.x, position.y, 20, 20);
+            position.xMin += 24;
+
+            if (injectorIcon != null)
+            {
+                var savedColor = GUI.color;
+                GUI.color = property.objectReferenceValue == null ? savedColor : Color.green;
+                GUI.DrawTexture(iconRect, injectorIcon);
+                GUI.color = savedColor;
+            }
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+}
+#endif
---
+++ Assets/LightWeightDI/Editor/InjectorEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+namespace DependencyInjection
+{
+    [CustomEditor(typeof(Injector))]
+    public class InjectorEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            var injector = (Injector) target;
+
+            if (GUILayout.Button("Validate Dependencies"))
+            {
+                injector.ValidateDependencies();
+            }
+
+            if (GUILayout.Button("Clear All Injectable Fields"))
+            {
+                injector.ClearDependencies();
+                EditorUtility.SetDirty(injector);
+            }
+        }
+    }
+}
+#endif
