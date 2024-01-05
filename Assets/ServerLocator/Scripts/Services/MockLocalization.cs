e103f32df1ab6327ce057b4b988a9b21fd10bcdd
Echo Yin <echo_ybl@hotmail.com>
2024-01-05 15:50:32 -0500

    Add mock services to testing

---
+++ Assets/ServerLocator/Scripts/Services/MockLocalization.cs
@@ -0,0 +1,16 @@
+﻿using ServerLocator.Scripts.Interfaces;
+using UnityEngine;
+
+namespace ServerLocator.Scripts.Services
+{
+    public class MockLocalization : ILocalization
+    {
+        const string Message = "MockLocalization.GetLocalizedWord with word ";
+        public string GetLocalizedWord(string key)
+        {
+            var word = $"{Message} {key}";
+            Debug.Log(word);
+            return word;
+        }
+    }
+}
