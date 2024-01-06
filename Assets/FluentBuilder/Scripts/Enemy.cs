6c7d110cab31357c4a428904f94157e8e44b2c33
Echo Yin <echo_ybl@hotmail.com>
2024-01-06 11:01:00 -0500

    basic setups and tests

---
+++ Assets/FluentBuilder/Scripts/Enemy.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+namespace FluentBuilder.Scripts
+{
+    public class Enemy : MonoBehaviour
+    {
+        public string Name { get; private set; }
+        public int Health { get; private set; }
+        public float Speed { get; private set; }
+        public int Damage { get; private set; }
+        public bool IsBoos { get; private set; }
+    
+        public class Builder
+        {
+            private string _name;
+            private int _health;
+            private float _speed;
+            private int _damage;
+            private bool _isBoss;
+
+            public Builder WithName(string name)
+            {
+                _name = name;
+                return this;
+            }
+
+            public Builder WithHealth(int health)
+            {
+                _health = health;
+                return this;
+            }
+
+            public Builder WithSpeed(float speed)
+            {
+                _speed = speed;
+                return this;
+            }
+
+            public Builder WithDamange(int damange)
+            {
+                _damage = damange;
+                return this;
+            }
+
+            public Builder WithIsBoss(bool isBoss)
+            {
+                _isBoss = isBoss;
+                return this;
+            }
+
+            public Enemy Build()
+            {
+                var enemy = new GameObject($"Enemy{(string.IsNullOrEmpty(_name) ? "Nameless" : _name)}")
+                    .AddComponent<Enemy>();
+                enemy.Name = _name;
+                enemy.Health = _health;
+                enemy.Speed = _speed;
+                enemy.Damage = _damage;
+                enemy.IsBoos = _isBoss;
+                return enemy;
+            }
+        }
+    }
+}
+
+
---
+++ Assets/FluentBuilder/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace FluentBuilder.Scripts
+{
+    public class GameManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            var enemy = new Enemy.Builder()
+                .WithName("Kobolt")
+                .WithDamange(10)
+                .WithSpeed(3f)
+                .WithHealth(20)
+                .WithIsBoss(false)
+                .Build();
+            
+            // Instantiate creates a clone
+            Instantiate(enemy,Vector3.zero,Quaternion.identity);
+        }
+    }
+}
