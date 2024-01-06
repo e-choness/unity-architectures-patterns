11ac09326eb077008f671e7304d30b1c3be7f874
Echo Yin <echo_ybl@hotmail.com>
2024-01-05 22:03:52 -0500

    Core components of service locator

---
+++ Assets/ServerLocator/Scripts/Components/BootStrapper.cs
@@ -0,0 +1,44 @@
+﻿using ClassExtensions.Scripts.Utilities;
+using UnityEngine;
+
+namespace ServerLocator.Scripts.Components
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(ServiceLocator))]
+    public abstract class Bootstrapper : MonoBehaviour
+    {
+        private ServiceLocator _container;
+        internal ServiceLocator Container => _container.OrNull() ?? (_container = GetComponent<ServiceLocator>());
+
+        private bool _hasBootstrapped;
+        private void Awake() => Bootstrap();
+
+        public void BootstrapOnDemand()
+        {
+            if (_hasBootstrapped) return;
+            _hasBootstrapped = true;
+            Bootstrap();
+        }
+
+        protected abstract void Bootstrap();
+    }
+
+    [AddComponentMenu("ServiceLocator/ServiceLocatorGlobal")]
+    public class GlobalBootstrapper : Bootstrapper
+    {
+        [SerializeField] private bool dontDestroyOnLoad = true;
+        protected override void Bootstrap()
+        {
+            Container.ConfigureAsGlobal(dontDestroyOnLoad);
+        }
+    }
+
+    [AddComponentMenu("ServiceLocator/ServiceLocatorScene")]
+    public class SceneBootstrapper : Bootstrapper
+    {
+        protected override void Bootstrap()
+        {
+            Container.ConfigureForScene();
+        }
+    }
+}
